b04a2a3199dd97c34b43d16adaab96898c19cab2
wuyiguang <376906177@qq.com>
2017-12-08 16:42:34 +0800

    更新依赖
    更新依赖

--- Example/Pods/Target Support Files/YGFileBrowser/YGFileBrowser-umbrella.h
+++ Example/Pods/Target Support Files/YGFileBrowser/YGFileBrowser-umbrella.h
@@ -10,6 +10,23 @@
 #endif
 #endif
 
+#import "CJFileManagerVC.h"
+#import "CJFlieLookUpVC.h"
+#import "CJFileObjModel.h"
+#import "CJHttp.h"
+#import "CJFileTools.h"
+#import "HSDownloadManager.h"
+#import "HSSessionModel.h"
+#import "NSString+Hash.h"
+#import "UIColor+CJColorCategory.h"
+#import "UIImage+TYHSetting.h"
+#import "UIView+CJToast.h"
+#import "UIView+LCCategory.h"
+#import "CJDownFileView.h"
+#import "VeFileDepartmentView.h"
+#import "VeFileManagerToolBar.h"
+#import "VeFileViewCell.h"
+#import "VeUnOpenFileView.h"
 
 FOUNDATION_EXPORT double YGFileBrowserVersionNumber;
 FOUNDATION_EXPORT const unsigned char YGFileBrowserVersionString[];
